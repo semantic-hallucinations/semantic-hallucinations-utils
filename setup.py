f935bebc1c9fdc8fd42c235e75eb4d187ebde8f5
Mediano4e <115105818+Mediano4e@users.noreply.github.com>
2025-04-22 06:13:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="organisation_utils",
-    version="0.1.0",
+    version="0.2.0",
     packages=['organisation_utils'],
     include_package_data=True,
     install_requires=[
