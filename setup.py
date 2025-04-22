69ac611fc7d95903c8316d2d3dfd6a0cfc9fdca0
Mediano4e <115105818+Mediano4e@users.noreply.github.com>
2025-04-22 06:21:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="organisation_utils",
     version="0.3.0",
-    packages=['organisation_utils'],
+    packages=['organisation_utils', 'logging_config', 'settings'],
     include_package_data=True,
     install_requires=[
         "pydantic",
